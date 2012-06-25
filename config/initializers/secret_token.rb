d45ef3f28e206f7d76abb66bc035dff5e351641c
Tomasz Nazar <tomasz.nazar@gmail.com>
2012-06-25 02:43:50 +0200

    gtfo rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AspectizedPetStore::Application.config.secret_token = '6a5e6e328dfd8e03fa77289eca67e2ae998bfcd952074cb9f76170b4792855b9baf9f12b9349f5b6764ce6243fa5676772a3c903a35e5adb7acc58c58dec5141'
